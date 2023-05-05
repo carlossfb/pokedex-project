5e8cdc26744fe7e7bf099f283e6247c620c77d14
Carlos Eduardo <72090014+carlossfb@users.noreply.github.com>
2023-05-04 22:40:27 -0300

    botao add pokemons

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,25 +1,34 @@
-
-function pokemonToHtml(pokemon){
-    return `
-        <li class="pokemon-card ${pokemon.type}">
-            <span class="number">${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-                <div class="pokemon-img">
-                    <img src="${pokemon.photo}" alt="${pokemon.name}">
-                </div>
-            </div>
-        </li>    
-    `
-}
 const pokemonHtml = document.getElementById("pokemon-list")
+const loadMoreButton = document.getElementById("loadMoreButton")
+const limit = 5
+let offset = 0
 
-pokeApi.getPokemons().then((pokemonList = []) => {
-    
-        pokemonHtml.innerHTML += pokemonList.map(pokemonToHtml).join('')
-        
+
+
+
+function loadPokemonItens(offset = 0, limit = 10){
+
+    pokeApi.getPokemons().then((pokemonList = []) => {
+        pokemonHtml.innerHTML += pokemonList.map((pokemon) =>
+        `<li class="pokemon-card ${pokemon.type}">
+                <span class="number">${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                         ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <div class="pokemon-img">
+                        <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    </div>
+                </div>
+            </li>`
+        ).join('')  
     })
-    
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () =>{
+    offset += limit
+    loadPokemonItens(offset, limit)
+})
